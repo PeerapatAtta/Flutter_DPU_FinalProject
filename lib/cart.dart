959d27f154e4788548890bda1ed314b67680da9c
OFF <peerapat13th@gmail.com>
2024-05-08 21:26:14 +0700

    Update Cart

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:my_aboutme/main.dart';
 
 class CartPage extends StatefulWidget {
@@ -18,16 +19,17 @@ class _CartPageState extends State<CartPage> {
         backgroundColor: color2,
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
+        child: Container(
+          padding: EdgeInsets.all(15),
+          alignment: Alignment.topCenter,
+          child: ListView.separated(
+            itemCount: 3,
+            itemBuilder: (ctx, i) => buildListTile(i),
+            separatorBuilder: (ctx, i) => Divider(
+              thickness: 1,
+              color: Colors.indigo,
             ),
-            Text(
-              'Cart Page Detail',
-              textScaleFactor: 2.0,
-            ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -41,6 +43,52 @@ class _CartPageState extends State<CartPage> {
       ),
     );
   }
+
+  var cartItems = [
+    'Iced Matcha Latte',
+    'Coffee White Russian',
+    'Hot Chocolate'
+  ];
+
+  Widget buildListTile(int index) => ListTile(
+        contentPadding: EdgeInsets.only(top: 5, bottom: 5),
+        leading: Icon(Icons.coffee_maker),
+        title: Text(cartItems[index]),
+        trailing: rowQuantity(index),
+      );
+
+  Widget rowQuantity(int index) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          OutlinedButton(
+              style: btnStyle(),
+              onPressed: () => (),
+              child: Text(
+                '+',
+                textScaleFactor: 1.3,
+              )),
+          Text(
+            '  1  ',
+            textScaleFactor: 1.4,
+          ),
+          OutlinedButton(
+              style: btnStyle(),
+              onPressed: () => (),
+              child: Text(
+                '-',
+                textScaleFactor: 1.3,
+              )),
+        ],
+      );
+
+  ButtonStyle btnStyle() => OutlinedButton.styleFrom(
+        minimumSize: Size(30, 30),
+        side: BorderSide(
+            color: Colors.black26, width: 1.2, style: BorderStyle.solid),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      );
 
   List<BottomNavigationBarItem> bottomNavItems() {
     var itemIcons = [
